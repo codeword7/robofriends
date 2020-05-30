0aaebf876cec369b77caa054869511fcaea1eec8
Neeraj Yadav <ny29599@gmail.com>
2020-05-30 10:59:11 +0530

    Updates

---
+++ precache-manifest.d8491b7e8e2198620fc00505d338db33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f445228c1565e41ccfb0e785c03b3ca6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "98ea22f1037b2e68ff46",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ea22f1037b2e68ff46",
+    "url": "/robofriends/static/js/main.e1716f42.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
